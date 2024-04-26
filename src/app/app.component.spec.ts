db0ee30d62610e73459b6892a2adcf8a242115a7
AnthonyNunez500 <u202119276@upc.edu.pe>
2024-04-26 00:56:01 -0500

    Commit 3
    - Cambio de logo en header principal
    - Cambio de nombre a CineFriend

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TuCine'`, () => {
+  it(`should have as title 'FriendCine'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TuCine');
+    expect(app.title).toEqual('FriendCine');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TuCine app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FriendCine app is running!');
   });
 });
